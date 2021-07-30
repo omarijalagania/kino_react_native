c4411cb6ad0a260c37388c850d77e64c54628db0
Irakli <koregm@gmail.com>
2021-07-30 11:35:06 +0400

    test commit

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 const ComingSoon = ({ data }) => {
-  return <Text style={styles.comingSoon}>Coming Soon...</Text>;
+  return <Text style={styles.comingSoon}>Coming Soon....</Text>;
 };
 
 const styles = StyleSheet.create({
