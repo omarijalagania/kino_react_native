61d67815ddad4d754c6ed0318374b0e6d7fe87ad
omari <omari.jalagania@gmail.com>
2021-12-13 16:05:12 +0400

    small hall

--- components/Hall.js
+++ components/Hall.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import SmallHall from "./SmallHall";
+
 import moment from "moment";
 
 const Hall = ({ navigation }) => {
@@ -10,6 +12,7 @@ const Hall = ({ navigation }) => {
     <View>
       <Text>{chosenTime}</Text>
       <Text>{moment(chosenDate).utc().local().format("DD.MM.YYYY")}</Text>
+      <SmallHall />
     </View>
   );
 };
---
+++ components/SmallHall.js
@@ -0,0 +1,270 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+
+export class SmallHall extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={styles.hallContanier}>
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1A</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1B</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2A</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2B</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3A</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3B</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3C</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4A</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4B</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5A</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5B</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* ------ */}
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>7/8</Text>
+            </TouchableOpacity>
+          </View>
+          {/* ------ */}
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>6/8</Text>
+            </TouchableOpacity>
+          </View>
+          {/* ------ */}
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>5/8</Text>
+            </TouchableOpacity>
+          </View>
+          {/* ------ */}
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>4/8</Text>
+            </TouchableOpacity>
+          </View>
+          {/* ------ */}
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>3/8</Text>
+            </TouchableOpacity>
+          </View>
+          {/* ------ */}
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>2/8</Text>
+            </TouchableOpacity>
+          </View>
+          {/* ------ */}
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.chair}>
+              <Text>1/8</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default SmallHall;
+
+//styles
+
+const styles = StyleSheet.create({
+  hallContanier: {},
+  row: {
+    padding: 2,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
+  chair: {
+    width: 30,
+    height: 30,
+    margin: 3,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "orange",
+  },
+});
